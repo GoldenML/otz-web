7d4024a984623618fdba174f86caca9369f37aee
shadowgao <shadowgao@tencent.com>
2023-07-05 15:49:05 +0800

    增加更多群信息显示

--- src/views/console/chats/chat-modal/index.vue
+++ src/views/console/chats/chat-modal/index.vue
@@ -1,123 +1,135 @@
-<template>
-  <div v-if="drawerVisible" id="drawer" ref="drawerRef" class="drawer">
+<template >
+  <div v-if="drawerVisible" id="drawer" ref="drawerRef" class="drawer" >
     <Drawer :username="username" />
-  </div>
-  <div>
-    <div class="chat-top">
+  </div >
+  <div >
+    <div class="chat-top" >
       {{ store.msgs[username]?.nickname }}
-      <div v-if="store.msgs[username].type === 2 " style="float: right;margin-right: 20px;cursor:pointer;" @click.stop="drawerVisible = true">
-        <el-icon><MoreFilled /></el-icon>
-      </div>
-    </div>
+      <span v-if="store.msgs[username].type === 2 && store.groupMember[username]" >
+    ({{ Object.keys(store.groupMember[username]).length }})
+  </span >
+      <div v-if="store.msgs[username].type === 2 " style="float: right;margin-right: 20px;cursor:pointer;"
+           @click.stop="drawerVisible = true" >
+        <el-icon >
+          <MoreFilled />
+        </el-icon >
+      </div >
+    </div >
 
-    <div id="chat-message" class="chat-message">
-      <div v-if="store.msgs[username].type === 1">
-        <div v-for="msg in store.msgs[username].msgList" :key="msg.sequence">
-          <template v-if="msg.from_username === store.msgs[username].username">
-            <div style="text-align: center; font-size: 12px;color: rgb(168,166,166)"> {{ msg.formatTime }}</div>
-            <div class="chat-message-left">
+    <div id="chat-message" class="chat-message" >
+      <div v-if="store.msgs[username].type === 1" >
+        <div v-for="msg in store.msgs[username].msgList" :key="msg.sequence" >
+          <template v-if="msg.from_username === store.msgs[username].username" >
+            <div style="text-align: center; font-size: 12px;color: rgb(168,166,166)" > {{ msg.formatTime }}</div >
+            <div class="chat-message-left" >
               <img
-                alt=""
-                style="float:left;vertical-align: middle; cursor: pointer"
-                :src="store.msgs[username].avatar"
-                :width="32"
-                :height="32"
-                @click.stop="handleShowInfo($event, false)"
+                  alt=""
+                  style="float:left;vertical-align: middle; cursor: pointer"
+                  :src="store.msgs[username].avatar"
+                  :width="32"
+                  :height="32"
+                  @click.stop="handleShowInfo($event, false)"
               >
-              <div v-if="msg.msg_type === 2" class="chat-message-left__img">
-                <img v-viewer :width="300" :src="msg.image_msg.image_url" alt="">
-              </div>
-              <div v-else class="chat-message-left__box">
+              <div v-if="msg.msg_type === 2" class="chat-message-left__img" >
+                <img v-viewer :width="300" :src="msg.image_msg.image_url" alt="" >
+              </div >
+              <div v-else class="chat-message-left__box" >
                 {{ msg.text_msg?.text }}
-              </div>
-            </div>
-          </template>
-          <template v-else>
-            <div style="text-align: center; font-size: 12px;color: rgb(168,166,166)"> {{ msg.formatTime }}</div>
-            <div class="chat-message-right">
-              <el-icon v-if="msg.wait" class="is-loading" style="margin-right: 5px;vertical-align: middle">
+              </div >
+            </div >
+          </template >
+          <template v-else >
+            <div style="text-align: center; font-size: 12px;color: rgb(168,166,166)" > {{ msg.formatTime }}</div >
+            <div class="chat-message-right" >
+              <el-icon v-if="msg.wait" class="is-loading" style="margin-right: 5px;vertical-align: middle" >
                 <Loading />
-              </el-icon>
-              <div v-if="msg.msg_type === 2" class="chat-message-right__img">
-                <img v-viewer :width="300" :src="msg.image_msg.image_url" alt="">
-              </div>
-              <div v-else class="chat-message-right__box">
+              </el-icon >
+              <div v-if="msg.msg_type === 2" class="chat-message-right__img" >
+                <img v-viewer :width="300" :src="msg.image_msg.image_url" alt="" >
+              </div >
+              <div v-else class="chat-message-right__box" >
                 {{ msg.text_msg?.text }}
-              </div>
+              </div >
               <img
-                alt=""
-                style="float: right; vertical-align: middle; cursor: pointer"
-                :src="store.userInfo.avatar"
-                :width="32"
-                :height="32"
-                @click.stop="handleShowInfo($event, true)"
+                  alt=""
+                  style="float: right; vertical-align: middle; cursor: pointer"
+                  :src="store.userInfo.avatar"
+                  :width="32"
+                  :height="32"
+                  @click.stop="handleShowInfo($event, true)"
               >
-            </div>
-          </template>
-        </div>
-      </div>
-      <div v-else-if="store.msgs[username].type === 2">
-        <div v-for="msg in store.msgs[username].msgList" :key="msg.username">
-          <template v-if="msg.isSystemMsg">
-            <div style="text-align: center; font-size: 12px;color: rgb(198, 173, 173)"><div style="text-align: center; font-size: 12px;color: rgb(168,166,166)"> {{ msg.formatTime }}</div> {{ msg.text_msg?.text }}</div>
-          </template>
-          <template v-else-if="msg.from_username === store.userInfo.username">
-            <div style="text-align: center; font-size: 12px;color: rgb(168,166,166)"> {{ msg.formatTime }}</div>
-            <div class="chat-message-right">
-              <el-icon v-if="msg.wait" class="is-loading" style="margin-right: 5px;vertical-align: middle">
+            </div >
+          </template >
+        </div >
+      </div >
+      <div v-else-if="store.msgs[username].type === 2" >
+        <div v-for="msg in store.msgs[username].msgList" :key="msg.username" >
+          <template v-if="msg.isSystemMsg" >
+            <div style="text-align: center; font-size: 12px;color: rgb(198, 173, 173)" >
+              <div style="text-align: center; font-size: 12px;color: rgb(168,166,166)" > {{ msg.formatTime }}</div >
+              {{ msg.text_msg?.text }}
+            </div >
+          </template >
+          <template v-else-if="msg.from_username === store.userInfo.username" >
+            <div style="text-align: center; font-size: 12px;color: rgb(168,166,166)" > {{ msg.formatTime }}</div >
+            <div class="chat-message-right" >
+              <el-icon v-if="msg.wait" class="is-loading" style="margin-right: 5px;vertical-align: middle" >
                 <Loading />
-              </el-icon>
-              <div v-if="msg.msg_type === 2" class="chat-message-right__img">
-                <img v-viewer :width="300" :src="msg.image_msg.image_url" alt="">
-              </div>
-              <div v-else class="chat-message-right__box">
+              </el-icon >
+              <div v-if="msg.msg_type === 2" class="chat-message-right__img" >
+                <img v-viewer :width="300" :src="msg.image_msg.image_url" alt="" >
+              </div >
+              <div v-else class="chat-message-right__box" >
                 {{ msg.text_msg?.text }}
-              </div>
+              </div >
               <img
-                alt=""
-                style="float: right; vertical-align: middle; cursor: pointer"
-                :src="store.userInfo.avatar"
-                :width="32"
-                :height="32"
-                @click.stop="handleShowInfo($event, true, true)"
+                  alt=""
+                  style="float: right; vertical-align: middle; cursor: pointer"
+                  :src="store.userInfo.avatar"
+                  :width="32"
+                  :height="32"
+                  @click.stop="handleShowInfo($event, true, true)"
               >
-            </div>
+            </div >
 
-          </template>
-          <template v-else>
-            <div style="text-align: center; font-size: 12px;color: rgb(168,166,166)"> {{ msg.formatTime }}</div>
-            <div class="chat-message-left">
+          </template >
+          <template v-else >
+            <div style="text-align: center; font-size: 12px;color: rgb(168,166,166)" > {{ msg.formatTime }}</div >
+            <div class="chat-message-left" >
 
               <img
-                alt=""
-                style="float:left;vertical-align: middle; cursor: pointer"
-                :src="store.groupMember[username][msg.from_username] ? store.groupMember[username][msg.from_username].avatar : store.cacheUser[msg.from_username]?.avatar"
-                :width="32"
-                :height="32"
-                @click.stop="handleShowInfo($event, false, true, msg.from_username)"
+                  alt=""
+                  style="float:left;vertical-align: middle; cursor: pointer"
+                  :src="store.groupMember[username][msg.from_username] ? store.groupMember[username][msg.from_username].avatar : store.cacheUser[msg.from_username]?.avatar"
+                  :width="32"
+                  :height="32"
+                  @click.stop="handleShowInfo($event, false, true, msg.from_username)"
               >
-              <div style="font-size: 12px; margin-left: 38px;position:relative; top: -8px;color: rgb(184, 184, 184)">{{ store.groupMember[username][msg.from_username] ? store.groupMember[username][msg.from_username].nickname : store.cacheUser[msg.from_username]?.nickname }} </div>
-              <div v-if="msg.msg_type === 2" class="chat-message-left__img">
-                <img v-viewer :width="300" :src="msg.image_msg.image_url" alt="">
-              </div>
-              <div v-else class="chat-message-left__box">
+              <div style="font-size: 12px; margin-left: 38px;position:relative; top: -8px;color: rgb(184, 184, 184)" >{{
+                  store.groupMember[username][msg.from_username] ? store.groupMember[username][msg.from_username].nickname : store.cacheUser[msg.from_username]?.nickname
+                }}
+              </div >
+              <div v-if="msg.msg_type === 2" class="chat-message-left__img" >
+                <img v-viewer :width="300" :src="msg.image_msg.image_url" alt="" >
+              </div >
+              <div v-else class="chat-message-left__box" >
                 {{ msg.text_msg?.text }}
-              </div>
+              </div >
 
-            </div>
-          </template>
-        </div>
-      </div>
+            </div >
+          </template >
+        </div >
+      </div >
 
       <div class="message-bottom" />
-    </div>
-    <div class="chat-tools">
+    </div >
+    <div class="chat-tools" >
       <el-popover
-        placement="top"
-        :width="300"
-        trigger="click"
+          placement="top"
+          :width="300"
+          trigger="click"
       >
-        <template #default>
+        <template #default >
           <!--          <div style="color: #000000; font-size: 14px">-->
           <!--            全部表情-->
           <!--          </div>-->
@@ -133,43 +145,44 @@
           <!--          </div>-->
           <!--          <Picker :data="emojiIndex" :native="true" set="twitter" @select="showEmoji" />-->
           <EmojiPicker :native="true" @select="onSelectEmoji" />
-        </template>
-        <template #reference>
-          <img width="30" height="30" alt="" src="@/assets/img/smile.png">
-        </template>
+        </template >
+        <template #reference >
+          <img width="30" height="30" alt="" src="@/assets/img/smile.png" >
+        </template >
 
-      </el-popover>
+      </el-popover >
 
-    </div>
+    </div >
     <div class="chat-content" />
-    <div class="chat-input">
+    <div class="chat-input" >
       <el-input
-        id="edit"
-        v-model="message"
-        :autosize="{ minRows: 7, maxRows: 7 }"
-        resize="none"
-        type="textarea"
-        @keydown.enter.exact.prevent="sendMessage"
+          id="edit"
+          v-model="message"
+          :autosize="{ minRows: 7, maxRows: 7 }"
+          resize="none"
+          type="textarea"
+          @keydown.enter.exact.prevent="sendMessage"
       />
-    </div>
-    <div style="text-align: right; padding-right: 20px; margin-top:  13px; position: absolute; bottom: 15px; right: 20px">
-      <el-button ref="buttonRef" class="btn-send" @click="sendMessage">发送</el-button>
+    </div >
+    <div
+        style="text-align: right; padding-right: 20px; margin-top:  13px; position: absolute; bottom: 15px; right: 20px" >
+      <el-button ref="buttonRef" class="btn-send" @click="sendMessage" >发送</el-button >
       <el-popover
-        ref="popoverRef"
-        :visible="visible"
-        :virtual-ref="buttonRef"
-        trigger="click"
-        virtual-triggering
-        :auto-close="2000"
-        placement="top-start"
+          ref="popoverRef"
+          :visible="visible"
+          :virtual-ref="buttonRef"
+          trigger="click"
+          virtual-triggering
+          :auto-close="2000"
+          placement="top-start"
       >
-        <span> 不能发送空白消息 </span>
-      </el-popover>
-    </div>
-  </div>
+        <span > 不能发送空白消息 </span >
+      </el-popover >
+    </div >
+  </div >
   <UserInfo ref="userInfoRef" />
-</template>
-<script setup lang="js">
+</template >
+<script setup lang="js" >
 import {
   defineAsyncComponent,
   onMounted,
@@ -190,7 +203,7 @@ import _ from 'lodash'
 import {CircleClose, Loading, MoreFilled} from '@element-plus/icons-vue'
 import data from 'emoji-mart-vue-fast/data/all.json'
 import 'emoji-mart-vue-fast/css/emoji-mart.css'
-import { Picker, EmojiIndex } from 'emoji-mart-vue-fast/src'
+import {Picker, EmojiIndex} from 'emoji-mart-vue-fast/src'
 import EmojiPicker from 'vue3-emoji-picker'
 import Drawer from '../components/Drawer.vue'
 import 'vue3-emoji-picker/css'
@@ -216,7 +229,7 @@ watch(props, () => {
   message.value = store.messages[props.username] || ''
 })
 onMounted(() => {
-  window.addEventListener('paste',handlePaste)
+  window.addEventListener('paste', handlePaste)
   window.addEventListener('click', closeDrawer)
   waitForAllImagesToLoad().then(() => {
     document.getElementById('chat-message').scrollTop = document.getElementById('chat-message').scrollHeight
@@ -233,13 +246,13 @@ onUpdated(() => {
   })
 })
 onUnmounted(() => {
-  window.removeEventListener('paste',handlePaste)
+  window.removeEventListener('paste', handlePaste)
   window.removeEventListener('click', closeDrawer)
 })
 const handlePaste = (e) => {
   let focusedElement = document.activeElement
-  if(focusedElement?.tagName === 'TEXTAREA') {
-    if(e.clipboardData.items.length > 0) {
+  if (focusedElement?.tagName === 'TEXTAREA') {
+    if (e.clipboardData.items.length > 0) {
       for (let i = 0; i < e.clipboardData.items.length; i++) {
         const item = e.clipboardData.items[i]
         if (item.kind === 'file' && item.type.indexOf('image') !== -1) {
@@ -331,7 +344,7 @@ const closeDrawer = (e) => {
 }
 
 
-const { proxy } = getCurrentInstance()
+const {proxy} = getCurrentInstance()
 const message = ref('')
 const emojis = reactive(emojiList)
 const addEmoji = (emoji) => {
@@ -403,35 +416,39 @@ const handleShowInfo = (e, left, isGroup, username) => {
   userInfoRef.value.handleShowInfo(e, left)
 }
 
-</script>
-<style lang="scss" scoped>
-.chat-top{
+</script >
+<style lang="scss" scoped >
+.chat-top {
   padding-left: 20px;
   line-height: 60px;
   height: 60px;
-  background-color: rgb(245,245,245);
+  background-color: rgb(245, 245, 245);
 }
-.chat-message{
+
+.chat-message {
   height: calc(800px - 300px);
   max-height: calc(800px - 300px);
   overflow: auto;
-  background-color: rgb(245,245,245);
-  &-left{
+  background-color: rgb(245, 245, 245);
+
+  &-left {
     margin-top: 10px;
     margin-left: 30px;
     padding-bottom: 10px;
-    &__img{
+
+    &__img {
       text-align: left;
       display: inline-block;
       line-height: 32px;
       min-height: 32px;
       margin-left: 5px;
-      padding:0 10px;
-      border-radius:5px;
+      padding: 0 10px;
+      border-radius: 5px;
       font-size: 14px;
       white-space: pre-wrap;
     }
-    &__box{
+
+    &__box {
       max-width: 500px;
       text-align: left;
       display: inline-block;
@@ -439,29 +456,32 @@ const handleShowInfo = (e, left, isGroup, username) => {
       min-height: 32px;
       background-color: rgb(255, 255, 255);
       margin-left: 5px;
-      padding:0 10px;
-      border-radius:5px;
+      padding: 0 10px;
+      border-radius: 5px;
       font-size: 14px;
       white-space: pre-wrap;
     }
   }
-  &-right{
+
+  &-right {
     text-align: right;
     margin-top: 10px;
     margin-right: 30px;
     padding-bottom: 10px;
-    &__img{
+
+    &__img {
       text-align: left;
       display: inline-block;
       line-height: 32px;
       min-height: 32px;
       margin-right: 5px;
-      padding:0 10px;
-      border-radius:5px;
+      padding: 0 10px;
+      border-radius: 5px;
       font-size: 14px;
       white-space: pre-wrap;
     }
-    &__box{
+
+    &__box {
       max-width: 500px;
       text-align: left;
       display: inline-block;
@@ -469,35 +489,40 @@ const handleShowInfo = (e, left, isGroup, username) => {
       min-height: 32px;
       margin-right: 5px;
       background-color: rgb(149, 236, 105);
-      padding:0 10px;
-      border-radius:5px;
+      padding: 0 10px;
+      border-radius: 5px;
       font-size: 14px;
       white-space: pre-wrap;
     }
   }
 }
-.chat-tools{
+
+.chat-tools {
   display: flex;
   align-items: center;
   line-height: 40px;
   height: 40px;
   border-top: 1px solid #ded7d7;
   padding: 0 15px;
-  img{
+
+  img {
     cursor: pointer;
   }
 }
-.chat-input{
+
+.chat-input {
   padding: 0 20px;
   min-height: 100px;
   background-color: rgb(245, 245, 245);
 }
-.emoji{
+
+.emoji {
   cursor: pointer;
   margin: 4px 3px;
   display: inline-block;
   font-size: 20px;
 }
+
 :deep(.el-textarea__inner) {
   padding: 0;
   box-shadow: 0 0 0 0;
@@ -505,14 +530,16 @@ const handleShowInfo = (e, left, isGroup, username) => {
   max-height: 140px;
   background-color: rgb(245, 245, 245);
 }
+
 :deep(.el-textarea__inner:hover) {
   box-shadow: 0 0 0 0;
 }
+
 :deep(.el-textarea__inner:focus) {
   box-shadow: 0 0 0 0;
 }
 
-.drawer{
+.drawer {
   // .el-drawer__body{
   //  background-color: rgb(245, 245, 245);
   //}
@@ -522,10 +549,11 @@ const handleShowInfo = (e, left, isGroup, username) => {
   height: 100%;
   width: 250px;
   background-color: rgb(245, 245, 245);
-  border-left: 1px solid rgb(236 ,236 ,236);
-  :deep .el-drawer__header{
+  border-left: 1px solid rgb(236, 236, 236);
+
+  :deep .el-drawer__header {
     margin: 0;
     padding: 0;
   }
 }
-</style>
+</style >
